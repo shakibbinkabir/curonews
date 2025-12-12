24b18acf9057d41b67e88489ce64db8f14f82b3a
Shakib Bin Kabir <shakibbinkabir@gmail.com>
2025-12-12 19:02:03 +0600

    Remove frontend

--- frontend/src/lib/hooks/use-categories.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getCategories } from '@/lib/api/categories';
-
-export function useCategories() {
-  return useQuery({
-    queryKey: ['categories'],
-    queryFn: getCategories,
-  });
-}
